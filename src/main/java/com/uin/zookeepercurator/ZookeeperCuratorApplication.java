219ea0963e9ba6fcdca80c054b44f74a795af959
lin.liu <2686221966@qq.com>
2023-08-30 22:46:38 +0800

    init:zookeeper demo initcreate

--- src/main/java/com/uin/zookeepercurator/ZookeeperCuratorApplication.java
+++ src/main/java/com/uin/zookeepercurator/ZookeeperCuratorApplication.java
@@ -3,11 +3,14 @@ package com.uin.zookeepercurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author dingchuan
+ */
 @SpringBootApplication
 public class ZookeeperCuratorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ZookeeperCuratorApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ZookeeperCuratorApplication.class, args);
+  }
 
 }
--- src/test/java/com/uin/zookeepercurator/ZookeeperCuratorApplicationTests.java
+++ src/test/java/com/uin/zookeepercurator/ZookeeperCuratorApplicationTests.java
@@ -1,13 +1,87 @@
 package com.uin.zookeepercurator;
 
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
+import java.util.Arrays;
+import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class ZookeeperCuratorApplicationTests {
 
-    @Test
-    void contextLoads() {
+  @Resource
+  private CuratorFramework curator;
+
+  /**
+   * 创建节点
+   *
+   * @throws Exception
+   */
+  @Test
+  void createNode() throws Exception {
+    /*
+     * 添加持久节点 默认
+     * */
+    // String s1 = curator.create().forPath("/dev");
+    // System.out.println(String.format("curator create node :%s successfully.", s1));
+
+    // 创建临时序号节点
+    String s = curator.create().withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
+        .forPath("/localdev", "some-data".getBytes());
+    System.out.printf("curator create node :%s successfully.%n", s);
+
+    System.in.read();
+  }
+
+  /**
+   * 获取节点的值
+   */
+  @Test
+  void getNodeData() {
+    try {
+      byte[] bytes = curator.getData().forPath("/dev");
+      System.out.println("bytes:[]" + Arrays.toString(bytes));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+  }
+
+  @Test
+  void setNodeData() {
+    Stat stat = null;
+    try {
+      stat = curator.setData().forPath("/dev", "newMessage".getBytes());
+      log.info("stat:{}", JSONUtil.parse(stat));
+      byte[] bytes = curator.getData().forPath("/dev");
+      log.info("localdev data:{}", Arrays.toString(bytes));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  void createWithParent() throws Exception {
+    String pathWithParent = "/localdev/sub-node-1";
+    // 创建多级节点
+    String path = curator.create().creatingParentContainersIfNeeded().forPath(pathWithParent);
+    System.out.printf("curator create node :%s success!%n", path);
+  }
+
+  @Test
+   void deleteWithChild() throws Exception {
+    String path = "/localdev";
+    //删除节点的同时一并删除子节点
+    curator.delete().guaranteed().deletingChildrenIfNeeded().forPath(path);
+  }
+
+  @Test
+  void contextLoads() {
+  }
 
 }
