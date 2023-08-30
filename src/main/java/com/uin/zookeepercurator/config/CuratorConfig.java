d6bb62ae681c6daa7c7aa8a4a6125a7b1fcc60ff
lin.liu <2686221966@qq.com>
2023-08-30 22:47:22 +0800

    create:操作curator，创建和删除节点

--- src/main/java/com/uin/zookeepercurator/config/CuratorConfig.java
+++ src/main/java/com/uin/zookeepercurator/config/CuratorConfig.java
@@ -1,2 +1,28 @@
-package com.uin.zookeepercurator.config;public class CuratorConfig {
+package com.uin.zookeepercurator.config;
+
+import lombok.AllArgsConstructor;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dingchuan
+ */
+@AllArgsConstructor
+@Configuration
+public class CuratorConfig {
+
+  private final ZookeeperProperties zookeeperProperties;
+
+  @Bean(initMethod = "start")
+  public CuratorFramework curatorFramework() {
+    return CuratorFrameworkFactory.newClient(
+        zookeeperProperties.getConnectAddress(),
+        zookeeperProperties.getSessionTimeoutMs(), zookeeperProperties.getConnectionTimeoutMs(),
+        new RetryNTimes(zookeeperProperties.getMaxRetries(),
+            zookeeperProperties.getElapsedTimeMs()));
+  }
+
 }
--- src/main/java/com/uin/zookeepercurator/config/ZookeeperProperties.java
+++ src/main/java/com/uin/zookeepercurator/config/ZookeeperProperties.java
@@ -10,15 +10,28 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 @ConfigurationProperties(prefix = "curator")
-public class WrapperZKConfig {
+public class ZookeeperProperties {
 
-  private int retryCount;
-
+  /**
+   * 最大重试次数
+   */
+  private int maxRetries;
+  /**
+   * 重试间隔时间，毫秒
+   */
   private int elapsedTimeMs;
-
-  private String connectionString;
-
+  /**
+   * 连接地址
+   */
+  private String connectAddress;
+  /**
+   * 会话超时时间，毫秒
+   * <p>
+   * 只要在SessionTimeout超时时间内能够重新连接上集群中任意一台服务器，那么之前创建的会话仍然有效
+   */
   private int sessionTimeoutMs;
-
+  /**
+   * 连接超时时间，毫秒
+   */
   private int connectionTimeoutMs;
 }
